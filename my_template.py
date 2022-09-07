92b8a3e12d36b1f2e3c99879754e0bf8e9815850
tsutsu <tsutsu0928@gmail.com>
2022-09-07 22:49:07 +0900

    再帰の高速化コードを追加

--- my_template.py
+++ my_template.py
@@ -3,6 +3,12 @@ from sys import setrecursionlimit, stdin
 
 setrecursionlimit(10 ** 9)
 
+try:
+    import pypyjit  # type: ignore
+    pypyjit.set_param('max_unroll_recursion=-1')
+except Exception:
+    pass
+
 _tokens = (y for x in stdin for y in x.split())
 def read(): return next(_tokens)
 def iread(): return int(next(_tokens))
