273fa15d9996aff8adf1fe2f31bbf5e7c4b9a5fe
tsutsu <tsutsu0928@gmail.com>
2023-01-14 11:39:35 +0900

    pow関数を標準のものを使用するように変更

--- str_hash.py
+++ str_hash.py
@@ -5,7 +5,7 @@ class StrHash:
         self.h = self._init_hash()
 
     def get_hash(self, s, e):
-        v = self.h[e + 1] - (self.h[s] * self._mpow(100, e - s + 1, self.mod) % self.mod)
+        v = self.h[e + 1] - (self.h[s] * pow(100, e - s + 1, self.mod) % self.mod)
         v %= self.mod
         return v
 
@@ -18,21 +18,3 @@ class StrHash:
 
     def _ctoi(self, c):
         return ord(c) - ord('a') + 1
-
-    def _mpow(self, base, exp, mod):
-        if exp == 0:
-            return 1
-        a = [i for i, x in enumerate(f'{exp:b}'[::-1]) if x == '1']
-        i, j, p, ret = 1, 0, base, 1
-        if a[0] == 0:
-            j += 1
-            ret *= base
-            ret %= mod
-        while j < len(a):
-            p = (p ** 2) % mod
-            if i == a[j]:
-                ret *= p
-                ret %= mod
-                j += 1
-            i += 1
-        return ret
