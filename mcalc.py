7f7d4d06d5e9851ec2689323a630bd564fe18dad
tsutsu <tsutsu0928@gmail.com>
2023-01-14 11:45:51 +0900

    pow関数が標準でmod対応されているので修正

--- mcalc.py
+++ mcalc.py
@@ -1,24 +1,9 @@
 def mpow(base, exp, mod):
-    if exp == 0:
-        return 1
-    a = [i for i, x in enumerate(f'{exp:b}'[::-1]) if x == '1']
-    i, j, p, ret = 1, 0, base, 1
-    if a[0] == 0:
-        j += 1
-        ret *= base
-        ret %= mod
-    while j < len(a):
-        p = (p ** 2) % mod
-        if i == a[j]:
-            ret *= p
-            ret %= mod
-            j += 1
-        i += 1
-    return ret
+    return pow(base, exp, mod)  # 標準関数でmod対応されている
 
 
 def mdiv(n, r, mod):
-    return (n * mpow(r, mod - 2, mod)) % mod
+    return (n * pow(r, mod - 2, mod)) % mod
 
 
 def mcomb(n, r, mod):
@@ -27,7 +12,7 @@ def mcomb(n, r, mod):
     ret = 1
     for i in range(r):
         ret *= n - i
-        ret *= mpow(i + 1, mod - 2, mod)
+        ret *= pow(i + 1, mod - 2, mod)
         ret %= mod
     return ret
 
