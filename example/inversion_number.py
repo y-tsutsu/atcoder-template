bd4fbb5024a3ee79ed3b5667091f8da2dc98fb74
tsutsu <tsutsu0928@gmail.com>
2023-02-09 21:53:12 +0900

    転倒数の求め方サンプルを実装

---
+++ example/inversion_number.py
@@ -0,0 +1,13 @@
+class SortedSet:
+    pass
+
+
+def inv_num(a):
+    ''' 転倒数 '''
+    tot = 0
+    ss = SortedSet()
+    for v in a:
+        i = ss.index_right(v)
+        tot += len(ss) - i
+        ss.add(v)
+    return tot
