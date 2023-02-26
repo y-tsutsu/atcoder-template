0ee89c533fc6ccc48ca122b142f1c700d18c8a63
tsutsu <tsutsu0928@gmail.com>
2023-02-26 17:32:32 +0900

    Manhattan MSTのサンプルコードを追加

---
+++ example/manhattan_mst.py
@@ -0,0 +1,49 @@
+class SegTree:
+    pass
+
+
+def manhattan_mst(n, x, y):
+    INF = 10 ** 18
+    ret = [INF for _ in range(n)]
+
+    xy = [(i, j) for i, j in zip(x, y)]
+    xy.sort()
+    x = [x for x, _ in xy]
+    y = [y for _, y in xy]
+
+    def op(x, y): return min(x, y)
+    def e(): return INF
+
+    # xi > xj, yi > yj
+    st = SegTree(op, e, n)
+    for i in range(n):
+        v = st.prod(0, y[i] + 1)
+        if v != -INF:
+            ret[i] = min(ret[i], x[i] + y[i] + v)
+        st.set(y[i], -x[i] - y[i])
+
+    # xi > xj, yi < yj
+    st = SegTree(op, e, n)
+    for i in range(n):
+        v = st.prod(y[i], n)
+        if v != INF:
+            ret[i] = min(ret[i], x[i] - y[i] + v)
+        st.set(y[i], -x[i] + y[i])
+
+    # xi < xj, yi > yj
+    st = SegTree(op, e, n)
+    for i in range(n - 1, -1, -1):
+        v = st.prod(0, y[i] + 1)
+        if v != INF:
+            ret[i] = min(ret[i], -x[i] + y[i] + v)
+        st.set(y[i], x[i] - y[i])
+
+    # xi < xj, yi < yj
+    st = SegTree(op, e, n)
+    for i in range(n - 1, -1, -1):
+        v = st.prod(y[i], n)
+        if v != INF:
+            ret[i] = min(ret[i], -x[i] - y[i] + v)
+        st.set(y[i], x[i] + y[i])
+
+    return ret
