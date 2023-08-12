0f243a4a4858b619d2108a85f5d8c41a24a194cb
tsutsu <tsutsu0928@gmail.com>
2023-08-12 10:45:34 +0900

    連立方程式の算出関数を別ファイルに切り出し

--- gcd_lcm.py
+++ gcd_lcm.py
@@ -1,8 +1,6 @@
 from functools import reduce
 from math import gcd
 
-import numpy as np
-
 
 def my_gcd(*num):
     return reduce(gcd, num)
@@ -14,17 +12,3 @@ def lcm(a, b):
 
 def my_lcm(*nums):
     return reduce(lcm, nums)
-
-
-def system_of_equations(a, b, c, d, u, v):
-    lac = lcm(a, c)
-    _, b_, _, d_, u_, v_ = lac, b * lac // a, lac, d * lac // c, u * lac // a, v * lac // c
-    y = (u_ - v_) / (b_ - d_)
-    x = (u - b * y) / a
-    return x, y
-
-
-def system_of_equations_np(a, b, c, d, u, v):
-    m = np.array([[a, b], [c, d]])
-    n = np.array([u, v])
-    return np.linalg.solve(m, n)
