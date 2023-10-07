77b9415c773cf3de0d3bb3fc06f7bb560994a9dc
tsutsu <tsutsu0928@gmail.com>
2023-10-07 15:14:15 +0900

    マンハッタン距離関連の関数を更新

--- example/manhattan_dist.py
+++ example/manhattan_dist.py
@@ -5,11 +5,12 @@ class SegTree:
     pass
 
 
-def manhattan_mst(n, x, y):
+def manhattan_mst(n, xs, ys):
+    '''各座標から他座標のうちマンハッタン距離の最小値を算出'''
     INF = maxsize
     ret = [INF for _ in range(n)]
 
-    xy = [(i, j) for i, j in zip(x, y)]
+    xy = [(i, j) for i, j in zip(xs, ys)]
     xy.sort()
     x = [x for x, _ in xy]
     y = [y for _, y in xy]
@@ -49,4 +50,14 @@ def manhattan_mst(n, x, y):
             ret[i] = min(ret[i], -x[i] - y[i] + v)
         st.set(y[i], x[i] + y[i])
 
+    return ret
+
+
+def manhattan_max(xys):
+    '''全座標の組合せの中でマンハッタン距離の最大値を算出'''
+    # https://fukubutyo.web.fc2.com/Manhattan.html
+    p = [(x + y, x - y) for x, y in xys]  # 45度回転のルート2倍の座標
+    xs = [x for x, _ in p]
+    ys = [y for _, y in p]
+    ret = max(max(xs) - min(xs), max(ys) - min(ys))
     return ret
