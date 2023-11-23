6b22440f617baf437853e7b5c1ad0c272cb8256f
tsutsu <tsutsu0928@gmail.com>
2023-11-23 16:04:13 +0900

    ダブリングの実装をクラス化

--- example/doubling.py
+++ example/doubling.py
@@ -1,21 +1,18 @@
-MAX = 64
+class Doubling:
+    def __init__(self, a, max_=64):
+        n = len(a)
+        self._d = [a[:]]
+        self._max = max_
+        for _ in range(self._max - 1):
+            p = [0 for _ in range(n)]
+            for i in range(n):
+                p[i] = self._d[-1][self._d[-1][i]]
+            self._d.append(p)
 
-
-def doubling(a):
-    n = len(a)
-    d = [a[:]]
-    for _ in range(MAX - 1):
-        p = [0 for _ in range(n)]
-        for i in range(n):
-            p[i] = d[-1][d[-1][i]]
-        d.append(p)
-    return d
-
-
-def query(pos, n, d):
-    '''posをn回移動する'''
-    for i in range(MAX - 1, -1, -1):
-        if n >> i & 1 == 0:
-            continue
-        pos = d[i][pos]
-    return pos
+    def query(self, pos, n):
+        '''posをn回移動する'''
+        for i in range(self._max - 1, -1, -1):
+            if n >> i & 1 == 0:
+                continue
+            pos = self._d[i][pos]
+        return pos
