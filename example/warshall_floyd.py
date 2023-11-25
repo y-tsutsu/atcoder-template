747d924119ea76f2c5596094c886b68b3c71edca
tsutsu <tsutsu0928@gmail.com>
2023-11-25 09:54:52 +0900

    ワーシャルフロイドのサンプルコードを整理

--- example/warshall_floyd.py
+++ example/warshall_floyd.py
@@ -1,11 +1,10 @@
 from sys import maxsize
 
-INF = maxsize // 4  # ワーシャルフロイドでmaxsizeを使うと計算が遅くなるので注意
-
 n = 23  # nは頂点数
 m = 42  # mは辺数
 abc = [[0, 1, 2] for _ in range(m)]  # aからbへの距離cの辺数がm
 
+INF = maxsize // 4  # ワーシャルフロイドでmaxsizeを使うと計算が遅くなるので注意
 d = [[INF for _ in range(n)] for _ in range(n)]  # iからjへの最短経路テーブル（初期値はINF）
 for i in range(n):
     d[i][i] = 0  # 自分自身への移動は0
@@ -21,18 +20,14 @@ def warshall_floyd(n, d):
                 d[i][j] = min(d[i][j], d[i][k] + d[k][j])
 
 
-warshall_floyd(n, d)
+def exist_negative_cycle(d):
+    '''負閉路判定'''
+    for i in range(n):
+        if d[i][i] < 0:
+            return True
+    return False
 
-# 負閉路判定
-for i in range(n):
-    if d[i][i] < 0:
-        print('NEGATIVE CYCLE')
-        exit()
 
-# INF判定
-for i in range(n):
-    for j in range(n):
-        if d[i][j] >= INF // 2:
-            d[i][j] = 'INF'
-
-print(*d, sep='\n')
+def is_disconnected_vertex(d, i, j):
+    '''INF判定'''
+    return True if d[i][j] >= INF // 2 else False
