7626c6faaa71b733e67a5eb07c87ec18a3c1bccd
tsutsu <tsutsu0928@gmail.com>
2023-11-26 14:38:53 +0900

    accumulateのinitialを使用するように更新

--- accumu_two_dim.py
+++ accumu_two_dim.py
@@ -2,7 +2,7 @@ from itertools import accumulate
 
 
 def acm2dim(p):
-    q = [[0] + list(accumulate(x)) for x in p]
+    q = [list(accumulate(x, initial=0)) for x in p]
     q = [x for x in zip(*q)]
-    q = [[0] + list(accumulate(x)) for x in q]
+    q = [list(accumulate(x, initial=0)) for x in q]
     return [list(x) for x in zip(*q)]
