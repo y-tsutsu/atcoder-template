26433f02bc6c1f591f2fad66f4d1c3ab40a4c12a
tsutsu <tsutsu0928@gmail.com>
2023-11-29 22:01:43 +0900

    2次元累積和の算出コードを高速化

--- accumu_two_dim.py
+++ accumu_two_dim.py
@@ -1,8 +1,10 @@
-from itertools import accumulate
-
-
 def acm2dim(p):
-    q = [list(accumulate(x, initial=0)) for x in p]
-    q = [x for x in zip(*q)]
-    q = [list(accumulate(x, initial=0)) for x in q]
-    return [list(x) for x in zip(*q)]
+    h, w = len(p), len(p[0])
+    q = [[0 for _ in range(w + 1)] for _ in range(h + 1)]
+    for i in range(h):
+        for j in range(w):
+            q[i + 1][j + 1] = q[i + 1][j] + p[i][j]
+    for j in range(w + 1):
+        for i in range(h):
+            q[i + 1][j] += q[i][j]
+    return q
