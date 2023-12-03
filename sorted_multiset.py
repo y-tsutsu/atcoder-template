6ee904d5e285b2202a253dd449782f4fd6640557
tsutsu <tsutsu0928@gmail.com>
2023-12-03 21:14:36 +0900

    privateな変数名を更新

--- sorted_multiset.py
+++ sorted_multiset.py
@@ -8,19 +8,19 @@ class SortedMultiset():
 
     def __init__(self, a=[]):
         a = list(a)
-        n = self.size = len(a)
+        n = self._size = len(a)
         if any(a[i] > a[i + 1] for i in range(n - 1)):
             a.sort()
         bucket_size = int(math.ceil(math.sqrt(n / self.BUCKET_RATIO)))
-        self.a = [a[n * i // bucket_size: n * (i + 1) // bucket_size] for i in range(bucket_size)]
+        self._a = [a[n * i // bucket_size: n * (i + 1) // bucket_size] for i in range(bucket_size)]
 
     def __iter__(self):
-        for i in self.a:
+        for i in self._a:
             for j in i:
                 yield j
 
     def __reversed__(self):
-        for i in reversed(self.a):
+        for i in reversed(self._a):
             for j in reversed(i):
                 yield j
 
@@ -28,23 +28,23 @@ class SortedMultiset():
         return list(self) == list(other)
 
     def __len__(self):
-        return self.size
+        return self._size
 
     def __repr__(self):
-        return 'SortedMultiset' + str(self.a)
+        return 'SortedMultiset' + str(self._a)
 
     def __str__(self):
         s = str(list(self))
         return '{' + s[1: len(s) - 1] + '}'
 
     def _position(self, x):
-        for i, a in enumerate(self.a):
+        for i, a in enumerate(self._a):
             if x <= a[-1]:
                 break
         return (a, i, bisect_left(a, x))
 
     def __contains__(self, x):
-        if self.size == 0:
+        if self._size == 0:
             return False
         a, _, i = self._position(x)
         return i != len(a) and a[i] == x
@@ -53,26 +53,26 @@ class SortedMultiset():
         return self.index_right(x) - self.index(x)
 
     def add(self, x):
-        if self.size == 0:
-            self.a = [[x]]
-            self.size = 1
+        if self._size == 0:
+            self._a = [[x]]
+            self._size = 1
             return
         a, b, i = self._position(x)
         a.insert(i, x)
-        self.size += 1
-        if len(a) > len(self.a) * self.SPLIT_RATIO:
+        self._size += 1
+        if len(a) > len(self._a) * self.SPLIT_RATIO:
             mid = len(a) >> 1
-            self.a[b:b+1] = [a[:mid], a[mid:]]
+            self._a[b:b+1] = [a[:mid], a[mid:]]
 
     def _pop(self, a, b, i):
         ans = a.pop(i)
-        self.size -= 1
+        self._size -= 1
         if not a:
-            del self.a[b]
+            del self._a[b]
         return ans
 
     def discard(self, x):
-        if self.size == 0:
+        if self._size == 0:
             return False
         a, b, i = self._position(x)
         if i == len(a) or a[i] != x:
@@ -81,33 +81,33 @@ class SortedMultiset():
         return True
 
     def lt(self, x):
-        for a in reversed(self.a):
+        for a in reversed(self._a):
             if a[0] < x:
                 return a[bisect_left(a, x) - 1]
 
     def le(self, x):
-        for a in reversed(self.a):
+        for a in reversed(self._a):
             if a[0] <= x:
                 return a[bisect_right(a, x) - 1]
 
     def gt(self, x):
-        for a in self.a:
+        for a in self._a:
             if a[-1] > x:
                 return a[bisect_right(a, x)]
 
     def ge(self, x):
-        for a in self.a:
+        for a in self._a:
             if a[-1] >= x:
                 return a[bisect_left(a, x)]
 
     def __getitem__(self, i):
         if i < 0:
-            for a in reversed(self.a):
+            for a in reversed(self._a):
                 i += len(a)
                 if i >= 0:
                     return a[i]
         else:
-            for a in self.a:
+            for a in self._a:
                 if i < len(a):
                     return a[i]
                 i -= len(a)
@@ -115,12 +115,12 @@ class SortedMultiset():
 
     def pop(self, i=-1):
         if i < 0:
-            for b, a in enumerate(reversed(self.a)):
+            for b, a in enumerate(reversed(self._a)):
                 i += len(a)
                 if i >= 0:
                     return self._pop(a, ~b, i)
         else:
-            for b, a in enumerate(self.a):
+            for b, a in enumerate(self._a):
                 if i < len(a):
                     return self._pop(a, b, i)
                 i -= len(a)
@@ -128,7 +128,7 @@ class SortedMultiset():
 
     def index(self, x):
         ans = 0
-        for a in self.a:
+        for a in self._a:
             if a[-1] >= x:
                 return ans + bisect_left(a, x)
             ans += len(a)
@@ -136,14 +136,14 @@ class SortedMultiset():
 
     def index_right(self, x):
         ans = 0
-        for a in self.a:
+        for a in self._a:
             if a[-1] > x:
                 return ans + bisect_right(a, x)
             ans += len(a)
         return ans
 
     def max(self):
-        return self.a[-1][-1]
+        return self._a[-1][-1]
 
     def min(self):
-        return self.a[0][0]
+        return self._a[0][0]
