cb10c49301e0871efb98c8b235a00750ff5e9669
tsutsu <tsutsu0928@gmail.com>
2024-02-11 14:30:55 +0900

    原点とその他2点の三角形の面積の算出関数を追加

--- geometry/geometry.py
+++ geometry/geometry.py
@@ -9,6 +9,10 @@ def calc_triangle_area(p1, p2, p3):
     return abs((p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])) / 2
 
 
+def calc_triangle_area_origin(p1, p2):
+    return abs(p1[0] * p2[1] - p1[1] * p2[0]) / 2
+
+
 def rotate_origin(x, y, radian):
     return x * cos(radian) - y * sin(radian), x * sin(radian) + y * cos(radian)
 
