d470dd6eba9d8a4fab1ea3e140a336a39949a636
tsutsu <tsutsu0928@gmail.com>
2024-02-23 22:04:35 +0900

    ユークリッドの互除法のgcdを実装

--- math/gcd_lcm.py
+++ math/gcd_lcm.py
@@ -1,8 +1,11 @@
 from functools import reduce
-from math import gcd
 
 
-def my_gcd(*num):
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a % b)
+
+
+def gcd_multi(*num):
     return reduce(gcd, num)
 
 
@@ -10,5 +13,5 @@ def lcm(a, b):
     return (a * b) // gcd(a, b)
 
 
-def my_lcm(*nums):
+def lcm_multi(*nums):
     return reduce(lcm, nums)
