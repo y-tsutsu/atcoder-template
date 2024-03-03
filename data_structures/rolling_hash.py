c24630a964ee77055f3b08164a78f8e6d0fc8c35
tsutsu <tsutsu0928@gmail.com>
2024-03-03 19:04:28 +0900

    セグメント木でのローリングハッシュを実装

--- data_structures/rolling_hash.py
+++ data_structures/rolling_hash.py
@@ -23,3 +23,55 @@ class RollingHash:
 
     def _ctoi(self, c):
         return ord(c) - ord('a') + 1
+
+
+class SegTree:
+    pass
+
+
+class SegRollingHash:
+    def __init__(self, s, mod=998244353, base=randint(100, 500)):
+        self._mod = mod
+        self._n = len(s)
+        self._pow = [1]
+        for _ in range(self._n):
+            self._pow.append(self._pow[-1] * base % self._mod)
+        self._st = self._init_hash(self._n)
+        for i, c in enumerate(s):
+            self.set(i, c)
+        self._pow = [1]
+        for _ in range(self._n):
+            self._pow.append(self._pow[-1] * base % self._mod)
+
+    def _init_hash(self, n):
+        def op(x, y):
+            (vx0, vx1, sx, ex), (vy0, vy1, sy, ey) = x, y
+            if sx == ex:
+                return y
+            if sy == ey:
+                return x
+            if sx > sy:
+                vx0, vx1, sx, ex, vy0, vy1, sy, ey = vy0, vy1, sy, ey, vx0, vx1, sx, ex
+            if ex == sy:
+                return [(vx0 * self._pow[ey - sy] + vy0) % self._mod,
+                        (vy1 * self._pow[ex - sx] + vx1) % self._mod,
+                        sx, ey]
+            if sx <= sy and ey <= ex:
+                return x
+            elif sy <= sx and ex <= ey:
+                return y
+            else:
+                assert False
+
+        def e(): return (0, 0, 0, 0)
+
+        return SegTree(op, e, n)
+
+    def get_hash(self, s, e):
+        '''（a ハッシュ， b 反転文字のハッシュ）: [s, e)'''
+        a, b, _, _ = self._st.prod(s, e)
+        return a, b
+
+    def set(self, p, c):
+        v = ord(c) - ord('a') + 1
+        self._st.set(p, (v, v, p, p + 1))
