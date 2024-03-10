d59ac1daa063ff8624b73b0f4122911ffc7b2aaf
tsutsu <tsutsu0928@gmail.com>
2024-03-10 16:58:49 +0900

    二部マッチングの超頂点とのフローの最大値を外部から設定できるように改善

--- graph/bipartite_matching.py
+++ graph/bipartite_matching.py
@@ -3,15 +3,15 @@ class Dinic:
 
 
 class BpMatch:
-    def __init__(self, n0, n1):
+    def __init__(self, n0, n1, v0=1, v1=1):
         self._n0 = n0
         self._n1 = n1
         self._n = n0 + n1
         self._dinic = Dinic(self._n + 2)
         for i in range(self._n0):
-            self._dinic.add_edge(self._n, i, 1)
+            self._dinic.add_edge(self._n, i, v0)
         for i in range(self._n1):
-            self._dinic.add_edge(self._n0 + i, self._n + 1, 1)
+            self._dinic.add_edge(self._n0 + i, self._n + 1, v1)
 
     def add_edge(self, fr, to, cap=1):
         self._dinic.add_edge(fr, self._n0 + to, cap)
