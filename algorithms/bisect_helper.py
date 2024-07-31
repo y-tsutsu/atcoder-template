540eacada1f00721a09145a3841bdc0213b710af
tsutsu <tsutsu0928@gmail.com>
2024-07-31 12:46:55 +0900

    二分探索まわりのHelper関数を追加

---
+++ algorithms/bisect_helper.py
@@ -0,0 +1,51 @@
+from bisect import bisect_left, bisect_right
+
+
+def lt(a, x):
+    i = bisect_left(a, x)
+    return None if i == 0 else a[i - 1]
+
+
+def le(a, x):
+    i = bisect_right(a, x)
+    return None if i == 0 else a[i - 1]
+
+
+def gt(a, x):
+    i = bisect_right(a, x)
+    return None if i == len(a) else a[i]
+
+
+def ge(a, x):
+    i = bisect_left(a, x)
+    return None if i == len(a) else a[i]
+
+
+def lt_count(a, x):
+    return bisect_left(a, x)
+
+
+def le_count(a, x):
+    return bisect_right(a, x)
+
+
+def gt_count(a, x):
+    return len(a) - bisect_right(a, x)
+
+
+def ge_count(a, x):
+    return len(a) - bisect_left(a, x)
+
+
+def range_count(a, lo, hi):
+    return bisect_right(a, hi) - bisect_left(a, lo)
+
+
+def range_inclusive_count(a, lo, hi):
+    '''[lo, hi]'''
+    return range_count(a, lo, hi)
+
+
+def range_exclusive_count(a, lo, hi):
+    '''[lo, hi)'''
+    return bisect_left(a, hi) - bisect_left(a, lo)
