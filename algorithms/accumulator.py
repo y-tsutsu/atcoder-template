ea23f7bfbe268d23a6c4c864db71e235b2eb76ba
tsutsu <tsutsu0928@gmail.com>
2024-08-11 02:30:57 +0900

    3次元の累積和の算出関数を追加

--- algorithms/accumulator.py
+++ algorithms/accumulator.py
@@ -12,16 +12,31 @@ def accumulate2dim(a):
     p = [[0 for _ in range(w + 1)] for _ in range(h + 1)]
     for i in range(h):
         for j in range(w):
-            p[i + 1][j + 1] = p[i + 1][j] + a[i][j]
-    for j in range(w + 1):
-        for i in range(h):
-            p[i + 1][j] += p[i][j]
+            p[i + 1][j + 1] = a[i][j] + p[i][j + 1] + p[i + 1][j] - p[i][j]
     return p
 
 
 def acm2dim_helper(a):
     acm = accumulate2dim(a)
     def helper(si, sj, ei, ej): return acm[ei][ej] - acm[si][ej] - acm[ei][sj] + acm[si][sj]
+    return helper
+
+
+def accumulate3dim(a):
+    d, h, w = len(a), len(a[0]), len(a[0][0])
+    p = [[[0 for _ in range(w + 1)] for _ in range(h + 1)] for _ in range(d + 1)]
+    for i in range(d):
+        for j in range(h):
+            for k in range(w):
+                p[i + 1][j + 1][k + 1] = (a[i][j][k] + p[i][j + 1][k + 1] + p[i + 1][j][k + 1] - p[i][j][k + 1]
+                                          + p[i + 1][j + 1][k] - p[i][j + 1][k] - p[i + 1][j][k] + p[i][j][k])
+    return p
+
+
+def acm3dim_helper(a):
+    acm = accumulate3dim(a)
+    def helper(si, sj, sk, ei, ej, ek): return (acm[ei][ej][ek] - acm[si][ej][ek] - acm[ei][sj][ek] + acm[si][sj][ek]
+                                                - (acm[ei][ej][sk] - acm[si][ej][sk] - acm[ei][sj][sk] + acm[si][sj][sk]))
     return helper
 
 
