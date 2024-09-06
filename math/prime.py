11261dfa82206e40ce9fa64eaf9a04799f1d4221
tsutsu <tsutsu0928@gmail.com>
2024-09-06 18:09:15 +0900

    変数名を変更

--- math/prime.py
+++ math/prime.py
@@ -1,18 +1,18 @@
 def prime_factorize(n):
-    a = []
+    ret = []
     while n % 2 == 0:
-        a.append(2)
+        ret.append(2)
         n //= 2
     f = 3
     while f * f <= n:
         if n % f == 0:
-            a.append(f)
+            ret.append(f)
             n //= f
         else:
             f += 2
     if n != 1:
-        a.append(n)
-    return a
+        ret.append(n)
+    return ret
 
 
 def is_prime(n):
