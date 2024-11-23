322d3c8cbbef45422d69461b8741a37e20f02079
tsutsu <tsutsu0928@gmail.com>
2024-11-23 12:24:20 +0900

    左側がTrueの場合の二分探索を追加

--- algorithms/bisect_lambda.py
+++ algorithms/bisect_lambda.py
@@ -1,12 +1,26 @@
-def bisect(s, e, is_right):
-    if not is_right(e):
+def bisect_ng_ok(s, e, is_ok):
+    if not is_ok(e):
         return e + 1
-    if is_right(s):
+    if is_ok(s):
         return s
     while e - s > 1:
         m = (e + s) // 2
-        if is_right(m):
+        if is_ok(m):
             e = m
         else:
             s = m
     return e
+
+
+def bisect_ok_ng(s, e, is_ok):
+    if is_ok(e):
+        return e
+    if not is_ok(s):
+        return s - 1
+    while e - s > 1:
+        m = (e + s) // 2
+        if is_ok(m):
+            s = m
+        else:
+            e = m
+    return s
