9d33d9890f5674969100e365823e5829f4e4e6a8
tsutsu <tsutsu0928@gmail.com>
2025-03-08 20:46:55 +0900

    Rangeのコードをリファクタリング

--- algorithms/range.py
+++ algorithms/range.py
@@ -13,7 +13,7 @@ class Range():
 
     def __str__(self): return f'({self.s}, {self.e})'
 
-    def __contains__(self, x): return self.s <= x < self.e
+    def __contains__(self, x): return self.s <= x < self.e if isinstance(x, int) else self.s <= x.s and x.e <= self.e if isinstance(x, Range) else False
 
     def is_overlaps(self, other): return max(self.s, other.s) < min(self.e, other.e)
 
@@ -26,36 +26,3 @@ class Range():
         if self.is_overlaps(other):
             return -1
         return abs(max(self.s, other.s) - min(self.e, other.e))
-
-    def contained_in(self, other):
-        return other.s <= self.s and self.e <= other.e
-
-
-def example():
-    r05 = Range(0, 5)
-    r04 = Range(0, 4)
-    for i in r05:
-        print(i)
-    for j in reversed(r05):
-        print(j)
-    print(r05 == Range(0, 5))
-    print(r05 == r04)
-    print(len(r05), len(r04))
-    print(len(Range(-5, 5)))
-    print(r05)
-    print(0 in r05)
-    print(5 in r05)
-    print(Range(0, 5).is_overlaps(Range(4, 10)), Range(0, 5).overlaps(Range(4, 10)))
-    print(Range(0, 5).is_overlaps(Range(5, 10)), Range(0, 5).overlaps(Range(5, 10)))
-    print(Range(5, 10).is_overlaps(Range(0, 6)), Range(5, 10).overlaps(Range(0, 6)))
-    print(Range(5, 10).is_overlaps(Range(0, 5)), Range(5, 10).overlaps(Range(0, 5)))
-    print(Range(0, 5).diff(Range(4, 10)))
-    print(Range(0, 5).diff(Range(5, 10)))
-    print(Range(0, 5).diff(Range(6, 10)))
-    print(Range(5, 10).diff(Range(0, 6)))
-    print(Range(5, 10).diff(Range(0, 5)))
-    print(Range(5, 10).diff(Range(0, 4)))
-
-
-if __name__ == '__main__':
-    example()
