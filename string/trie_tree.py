490f1aa943837edb2784b9bd79903527a7fc545a
tsutsu <tsutsu0928@gmail.com>
2025-04-30 15:41:25 +0900

    TrieTreeをリファクタリング

--- string/trie_tree.py
+++ string/trie_tree.py
@@ -15,17 +15,6 @@ class TrieTree:
         def is_terminate(self):
             return self.value is not None
 
-        def __eq__(self, other):
-            if not isinstance(other, TrieTree.Node):
-                return False
-            return self.char == other.char
-
-        def __hash__(self):
-            return hash(self.char)
-
-        def __str__(self):
-            return f'{self.char} {self.rank} {self.count} {self.value}'
-
     def __init__(self):
         self._root = TrieTree.Node()
 
@@ -36,23 +25,42 @@ class TrieTree:
         if pos == len(text):
             parent.value = text
             parent.count += 1
-            yield
+            yield parent
         parent.count += 1
         c = text[pos]
         if c not in parent.children:
             parent.children[c] = TrieTree.Node(char=c, rank=pos, parent=parent)
         node = parent.children[c]
-        yield self.insert(text, node, pos + 1)
-        yield
+        ret = yield self.insert(text, node, pos + 1)
+        yield ret
+
+    @recurboost
+    def lcp(self, text, parent=None, pos=0):
+        '''最長共通prefix'''
+        if parent is None:
+            parent = self._root
+        if pos == len(text):
+            yield text[:parent.rank + 1]
+        c = text[pos]
+        if c not in parent.children:
+            yield text[:parent.rank + 1]
+        child = parent.children[c]
+        if child.count == 1:
+            yield text[:parent.rank + 1]
+        ret = yield self.lcp(text, parent.children[c], pos + 1)
+        yield ret
 
     def search(self, text, parent=None, pos=0):
+        '''textが登録済みか？'''
         return self._inner_search(text, parent, pos, lambda p: p.is_terminate())
 
     def prefix_with(self, text, parent=None, pos=0):
-        return self._inner_search(text, parent, pos, lambda p: True)
+        '''textが登録された文字列のprefixとして存在するか？'''
+        return self._inner_search(text, parent, pos, lambda _: True)
 
     def common_prefix_with(self, text, parent=None, pos=0):
-        return self._inner_search(text, parent, pos, lambda p: (p.is_terminate() and p.count >= 2) or (not p.is_terminate() and p.count >= 2))
+        '''textが登録された文字列のうち複数の共通prefixとして存在するか？'''
+        return self._inner_search(text, parent, pos, lambda p: p.count >= 2)
 
     @recurboost
     def _inner_search(self, text, parent, pos, result_funk):
@@ -64,21 +72,6 @@ class TrieTree:
         if c not in parent.children:
             yield False
         ret = yield self._inner_search(text, parent.children[c], pos + 1, result_funk)
-        yield ret
-
-    @recurboost
-    def common_prefix(self, text, parent=None, pos=0):
-        if parent is None:
-            parent = self._root
-        if pos == len(text):
-            yield text[:parent.rank + 1]
-        c = text[pos]
-        if c not in parent.children:
-            yield text[:parent.rank + 1]
-        child = parent.children[c]
-        if child.count == 1:
-            yield text[:parent.rank + 1]
-        ret = yield self.common_prefix(text, parent.children[c], pos + 1)
         yield ret
 
 
@@ -93,7 +86,7 @@ def example():
     tr.insert('bb')
     tr.insert('bbb')
     tr.insert('ccc')
-    print('// count: //')
+    print('## count: ##')
     print(tr._root.count == 9)
     print(tr._root.children['a'].count == 5)
     print(tr._root.children['a'].children['b'].count == 5)
@@ -109,7 +102,7 @@ def example():
     print(tr._root.children['c'].count == 1)
     print(tr._root.children['c'].children['c'].count == 1)
     print(tr._root.children['c'].children['c'].children['c'].count == 1)
-    print(f'// search: {True} //')
+    print(f'## search: {True} ##')
     print(tr.search('abcd'))
     print(tr.search('abcc'))
     print(tr.search('abxyz'))
@@ -119,7 +112,7 @@ def example():
     print(tr.search('bb'))
     print(tr.search('bbb'))
     print(tr.search('ccc'))
-    print(f'// search: {False} //')
+    print(f'## search: {False} ##')
     print(tr.search('a'))
     print(tr.search('ab'))
     print(tr.search('abc'))
@@ -131,7 +124,7 @@ def example():
     print(tr.search('cc'))
     print(tr.search('c'))
     print(tr.search(''))
-    print(f'// prefix_with: {True} //')
+    print(f'## prefix_with: {True} ##')
     print(tr.prefix_with('abc'))
     print(tr.prefix_with('ab'))
     print(tr.prefix_with('a'))
@@ -147,10 +140,10 @@ def example():
     print(tr.prefix_with('abxyz'))
     print(tr.prefix_with('bbb'))
     print(tr.prefix_with('ccc'))
-    print(f'// prefix_with: {False} //')
+    print(f'## prefix_with: {False} ##')
     print(tr.prefix_with('bbbb'))
     print(tr.prefix_with('z'))
-    print(f'// common_prefix_with: {True} //')
+    print(f'## common_prefix_with: {True} ##')
     print(tr.common_prefix_with('abc'))
     print(tr.common_prefix_with('ab'))
     print(tr.common_prefix_with('a'))
@@ -159,7 +152,7 @@ def example():
     print(tr.common_prefix_with('bb'))
     print(tr.common_prefix_with('b'))
     print(tr.common_prefix_with(''))
-    print(f'// common_prefix_with: {False} //')
+    print(f'## common_prefix_with: {False} ##')
     print(tr.common_prefix_with('abcd'))
     print(tr.common_prefix_with('abcc'))
     print(tr.common_prefix_with('abxyz'))
@@ -169,19 +162,19 @@ def example():
     print(tr.common_prefix_with('cc'))
     print(tr.common_prefix_with('c'))
     print(tr.common_prefix_with('z'))
-    print(f'// common_prefix: {True} //')
-    print(tr.common_prefix('abcd') == 'abc')
-    print(tr.common_prefix('abcc') == 'abc')
-    print(tr.common_prefix('abxyz') == 'abxy')
-    print(tr.common_prefix('abx') == 'abx')
-    print(tr.common_prefix('abxy') == 'abxy')
-    print(tr.common_prefix('b') == 'b')
-    print(tr.common_prefix('bb') == 'bb')
-    print(tr.common_prefix('bbb') == 'bb')
-    print(tr.common_prefix('ccc') == '')
-    print(tr.common_prefix('abcz') == 'abc')
-    print(tr.common_prefix('az') == 'a')
-    print(tr.common_prefix('z') == '')
+    print(f'## common_prefix: {True} ##')
+    print(tr.lcp('abcd') == 'abc')
+    print(tr.lcp('abcc') == 'abc')
+    print(tr.lcp('abxyz') == 'abxy')
+    print(tr.lcp('abx') == 'abx')
+    print(tr.lcp('abxy') == 'abxy')
+    print(tr.lcp('b') == 'b')
+    print(tr.lcp('bb') == 'bb')
+    print(tr.lcp('bbb') == 'bb')
+    print(tr.lcp('ccc') == '')
+    print(tr.lcp('abcz') == 'abc')
+    print(tr.lcp('az') == 'a')
+    print(tr.lcp('z') == '')
 
 
 if __name__ == '__main__':
