bfcedf1494246f82567e14f75439f1ffe642117d
tsutsu <tsutsu0928@gmail.com>
2025-05-08 20:24:48 +0900

    全方位木DPのサンプル実装を追加

--- graph/tree_diameter.py
+++ graph/tree_diameter.py
@@ -48,3 +48,42 @@ def route(s, e, to):
     ret = []
     dfs(s, -1, ret)
     return ret
+
+
+def rerooting_dp(n, to):
+    '''全方位木DPでの木の直径算出'''
+    d = [0 for _ in range(n)]
+
+    @recurboost
+    def dfs(i, p):
+        ret = 0
+        for j in to[i]:
+            if j == p:
+                continue
+            r = yield dfs(j, i)
+            ret = max(ret, r + 1)
+        d[i] = ret
+        yield ret
+
+    dfs(0, -1)
+
+    @recurboost
+    def dfs2(i, p, pv):
+        a = [(0, -1)]  # 番兵
+        for j in to[i]:
+            if j == p:
+                a.append((pv + 1, j))
+            else:
+                a.append((d[j] + 1, j))
+        a.sort(reverse=True)
+        ret = sum([u for u, _ in a[:2]])
+        for j in to[i]:
+            if j == p:
+                continue
+            npv = a[0][0] if a[0][1] != j else a[1][0]
+            r = yield dfs2(j, i, npv)
+            ret = max(ret, r)
+        yield ret
+
+    ret = dfs2(0, -1, 0)
+    return ret
