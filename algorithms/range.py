2e94fcbdd2d91fca454adc0406dcca8469e855ac
tsutsu <tsutsu0928@gmail.com>
2025-12-07 00:26:10 +0900

    Rangeクラスの大小比較とマージ機能を追加

--- algorithms/range.py
+++ algorithms/range.py
@@ -1,7 +1,9 @@
+from functools import total_ordering
+
+
+@total_ordering
 class Range():
-    def __init__(self, s, e):
-        assert s <= e
-        self.s, self.e = s, e
+    def __init__(self, s, e): self.s, self.e = s, e
 
     def __iter__(self): return (i for i in range(self.s, self.e))
 
@@ -9,20 +11,18 @@ class Range():
 
     def __eq__(self, other): return self.s == other.s and self.e == other.e
 
+    def __lt__(self, other): return (self.s, self.e) < (other.s, other.e)
+
     def __len__(self): return self.e - self.s
 
     def __str__(self): return f'({self.s}, {self.e})'
 
     def __contains__(self, x): return self.s <= x < self.e if isinstance(x, int) else self.s <= x.s and x.e <= self.e if isinstance(x, Range) else False
 
-    def is_overlaps(self, other): return max(self.s, other.s) < min(self.e, other.e)
+    def overlaps(self, other): return max(self.s, other.s) < min(self.e, other.e)
 
-    def overlaps(self, other):
-        if not self.is_overlaps(other):
-            return Range(0, 0)
-        return Range(max(self.s, other.s), min(self.e, other.e))
+    def intersection(self, other): return Range(max(self.s, other.s), min(self.e, other.e)) if self.overlaps(other) else None
 
-    def diff(self, other):
-        if self.is_overlaps(other):
-            return -1
-        return abs(max(self.s, other.s) - min(self.e, other.e))
+    def union(self, other): return Range(min(self.s, other.s), max(self.e, other.e)) if self.overlaps(other) else None
+
+    def gap(self, other): return abs(max(self.s, other.s) - min(self.e, other.e)) if not self.overlaps(other) else -1
