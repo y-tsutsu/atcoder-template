76ea71243ff17a025c76d193995bee99283eebdd
tsutsu <tsutsu0928@gmail.com>
2025-12-29 18:54:04 +0900

    ポジションだけでなく重みの加算も行うダブリングのサンプルを実装

--- data_structures/doubling.py
+++ data_structures/doubling.py
@@ -16,3 +16,29 @@ class Doubling:
                 continue
             pos = self._d[i][pos]
         return pos
+
+
+class DoublingWeight:
+    def __init__(self, a, weight, max_=64):
+        n = len(a)
+        self._d = [a[:]]
+        self._e = [weight[:]]
+        self._max = max_
+        for _ in range(self._max - 1):
+            p = [0 for _ in range(n)]
+            q = [0 for _ in range(n)]
+            for i in range(n):
+                p[i] = self._d[-1][self._d[-1][i]]
+                q[i] = self._e[-1][i] + self._e[-1][self._d[-1][i]]
+            self._d.append(p)
+            self._e.append(q)
+
+    def query(self, pos, n):
+        '''posからn回移動する'''
+        tot = 0
+        for i in range(self._max - 1, -1, -1):
+            if n >> i & 1 == 0:
+                continue
+            tot += self._e[i][pos]
+            pos = self._d[i][pos]
+        return pos, tot
