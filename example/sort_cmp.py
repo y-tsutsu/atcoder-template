28f81b46a2da70748121a0f00b8311a3015870cf
tsutsu <tsutsu0928@gmail.com>
2026-01-25 14:40:54 +0900

    独自オブジェクトの座標圧縮を実装

--- example/sort_cmp.py
+++ example/sort_cmp.py
@@ -1,6 +1,8 @@
-from functools import cmp_to_key
+from bisect import bisect_left
+from functools import cmp_to_key, total_ordering
 
 
+@total_ordering
 class Comparable:
     '''sortの比較は__lt__()が定義されていればOK'''
 
@@ -8,7 +10,19 @@ class Comparable:
         pass
 
     def __lt__(self, other):
-        pass
+        return True
+
+    def __eq__(self, other):
+        return True
+
+
+def compress(a):
+    '''独自クラスでの比較は効率が悪いのでintで座標圧縮する関数'''
+    uniq = []
+    for x in sorted(a):
+        if not uniq or x != uniq[-1]:
+            uniq.append(x)
+    return [bisect_left(uniq, x) for x in a]
 
 
 def cmp(u, v):
