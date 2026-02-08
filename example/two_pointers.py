e656ea94061943ac828c1df2a924a1e7d94ed902
tsutsu <tsutsu0928@gmail.com>
2026-02-08 13:18:59 +0900

    尺取り法のテンプレートを実装

---
+++ example/two_pointers.py
@@ -0,0 +1,30 @@
+def main():
+    '''尺取り法のテンプレート'''
+
+    def ok(ri):
+        '''riを適用しても条件を満たすか判定'''
+        return True
+
+    def push(ri):
+        '''riを適用'''
+        pass
+
+    def pop(le):
+        '''leを除外'''
+        pass
+
+    n = 100
+    ans = 0
+    ri = 0
+    for le in range(n):
+        ri = max(le, ri)  # leの追い越しを防止
+        while ri != n and ok(ri):
+            push(ri)
+            ri += 1
+        ans += ri - le
+        pop(le)
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
