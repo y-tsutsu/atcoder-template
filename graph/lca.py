05609e33f656942bc00eac9e8d26a83c8a71bd86
tsutsu <tsutsu0928@gmail.com>
2026-03-29 13:06:11 +0900

    LCAのライブラリを改善

--- graph/lca.py
+++ graph/lca.py
@@ -1,61 +1,74 @@
-from collections import deque
+def recurboost(func=None, stack=[]):
+    pass
 
 
 class LCA:
-    def __init__(self, n, to, root=0):
-        self._m = (n - 1).bit_length()
-        self._prv, self._depth = self._initialize(n, to, root)
-        self._kprv = self._create_kprv(self._prv, n, self._m)
+    def __init__(self, n):
+        self._n = n
+        self._LOG = (n - 1).bit_length()
+        self._to = [[] for _ in range(n)]
+        self._parent = [[-1 for _ in range(n)] for _ in range(self._LOG)]
+        self._depth = [0 for _ in range(n)]
+        self._dist = [0 for _ in range(n)]
 
-    def _initialize(self, n, to, root):
-        prv = [None for _ in range(n)]
-        depth = [-1 for _ in range(n)]
-        depth[root] = 0
-        dq = deque()
-        dq.append(root)
-        while dq:
-            i = dq.popleft()
-            for j in to[i]:
-                if depth[j] != -1:
-                    continue
-                prv[j] = i
-                to[j].remove(i)
-                depth[j] = depth[i] + 1
-                dq.append(j)
-        return prv, depth
+    @recurboost
+    def _dfs(self, v, p, d, dist):
+        self._parent[0][v] = p
+        self._depth[v] = d
+        self._dist[v] = dist
+        for nv, w in self._to[v]:
+            if nv == p:
+                continue
+            yield self._dfs(nv, v, d + 1, dist + w)
+        yield
 
-    def _create_kprv(self, prv, n, m):
-        kprv = [prv]
-        p = prv
-        for _ in range(m):
-            q = [0 for _ in range(n)]
-            for i in range(n):
-                if p[i] is None:
-                    continue
-                q[i] = p[p[i]]
-            kprv.append(q)
-            p = q
-        return kprv
+    def add_edge(self, u, v, w=1):
+        self._to[u].append((v, w))
+        self._to[v].append((u, w))
+
+    def build(self, root=0):
+        self._dfs(root, -1, 0, 0)
+        for k in range(self._LOG - 1):
+            for v in range(self._n):
+                if self._parent[k][v] < 0:
+                    self._parent[k + 1][v] = -1
+                else:
+                    self._parent[k + 1][v] = self._parent[k][self._parent[k][v]]
 
     def lca(self, u, v):
-        dd = self._depth[v] - self._depth[u]
-        if dd < 0:
+        if self._depth[u] < self._depth[v]:
             u, v = v, u
-            dd = -dd
-        for k in range(self._m + 1):
-            if dd & 1:
-                v = self._kprv[k][v]
-            dd >>= 1
+        diff = self._depth[u] - self._depth[v]
+        for k in range(self._LOG):
+            if diff >> k & 1:
+                u = self._parent[k][u]
         if u == v:
             return u
-        for k in range(self._m - 1, -1, -1):
-            pu = self._kprv[k][u]
-            pv = self._kprv[k][v]
-            if pu != pv:
-                u = pu
-                v = pv
-        return self._kprv[0][u]
+        for k in reversed(range(self._LOG)):
+            if self._parent[k][u] != self._parent[k][v]:
+                u = self._parent[k][u]
+                v = self._parent[k][v]
+        return self._parent[0][u]
 
     def distance(self, u, v):
-        lca = self.lca(u, v)
-        return self._depth[u] + self._depth[v] - 2 * self._depth[lca]
+        x = self.lca(u, v)
+        return self._dist[u] + self._dist[v] - 2 * self._dist[x]
+
+    def kth_ancestor(self, v, k):
+        '''vのk個上の祖先'''
+        for i in range(self._LOG):
+            if v < 0:
+                return -1
+            if k >> i & 1:
+                v = self._parent[i][v]
+        return v
+
+    def kth_node(self, u, v, k):
+        '''u -> vのパス上でk番目のノード'''
+        x = self.lca(u, v)
+        d1 = self._depth[u] - self._depth[x]
+        if k <= d1:
+            return self.kth_ancestor(u, k)
+        else:
+            k2 = self._depth[v] - self._depth[x] - (k - d1)
+            return self.kth_ancestor(v, k2)
