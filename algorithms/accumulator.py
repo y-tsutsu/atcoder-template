e7fc090d0546b4ce7e9e5ab06391cbecc58b78a9
tsutsu <tsutsu0928@gmail.com>
2026-07-04 18:54:46 +0900

    関数名を変更

--- algorithms/accumulator.py
+++ algorithms/accumulator.py
@@ -60,35 +60,35 @@ def acm3dim_helper(a):
 def imos_helper(n):
     p = [0 for _ in range(n + 1)]
 
-    def set_helper(s, e, x):
+    def setter(s, e, x):
         p[s] += x
         p[e] -= x
 
-    def acm_helper(): return list(accumulate(p))[:-1]
+    def builder(): return list(accumulate(p))[:-1]
 
-    return set_helper, acm_helper
+    return setter, builder
 
 
 def imos2d_helper(h, w):
     p = [[0 for _ in range(w + 1)] for _ in range(h + 1)]
 
-    def set_helper(si, sj, ei, ej, x):
+    def setter(si, sj, ei, ej, x):
         for i, j, v in [(si, sj, x), (ei, sj, -x), (si, ej, -x), (ei, ej, x)]:
             p[i][j] += v
 
-    def acm_helper(): return accumulate2dim(p)
+    def builder(): return accumulate2dim(p)
 
-    return set_helper, acm_helper
+    return setter, builder
 
 
 def imos3d_helper(d, h, w):
     p = [[[0 for _ in range(w + 1)] for _ in range(h + 1)] for _ in range(d + 1)]
 
-    def set_helper(si, sj, sk, ei, ej, ek, x):
+    def setter(si, sj, sk, ei, ej, ek, x):
         for i, j, k, v in [(si, sj, sk, x), (ei, sj, sk, -x), (si, ej, sk, -x), (ei, ej, sk, x),
                            (si, sj, ek, -x), (ei, sj, ek, x), (si, ej, ek, x), (ei, ej, ek, -x)]:
             p[i][j][k] += v
 
-    def acm_helper(): return accumulate3dim(p)
+    def builder(): return accumulate3dim(p)
 
-    return set_helper, acm_helper
+    return setter, builder
